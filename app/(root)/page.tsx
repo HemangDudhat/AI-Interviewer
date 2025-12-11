1afe11e2a05f40e01c9f2b51f2cd3f124a8d0281
HemangDudhat <hemangdudhat@gmail.com>
2025-12-11 11:29:12 +0530

    Enhance layout and page structure, add InterviewCard component with feedback integration, and update dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
+import { Link } from 'next/link'
+import Image from 'next/image'
 function Page() {
   return (
-    <html>
-    
-    <body>
-      <div>Home Page</div>
-    </body>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
 
-    </html>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+    </>
   )
 }
 
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,12 +1,12 @@
-// Import the functions you need from the SDKs you need
+// Importing the functions needed from the SDKs
 import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: Added SDKs for Firebase products that are wanting to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
